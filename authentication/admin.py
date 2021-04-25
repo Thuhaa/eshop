f07252cdb7b41266683c6ef3a903b6ef5a2be878
Thuhaa <josephthuha@gmail.com>
2021-04-25 22:27:23 +0300

    Minor

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from .models import CustomUser
 
-
+#Class to display the fields in the admin page
 class CustomUsersAdmin(admin.ModelAdmin):
 	list_display = ['first_name','last_name','email']
 
+#Register the User model
 admin.site.register(CustomUser, CustomUsersAdmin)
+
+#Unregister the Group class
 admin.site.unregister(Group)
-# Register your models here.
