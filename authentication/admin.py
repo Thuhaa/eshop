40bd8369398e51a4115167ec220e5beb7e76d26a
Thuhaa <josephthuha@gmail.com>
2021-04-25 18:39:45 +0300

    Added a new custom user model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import CustomUser
 
+
+class CustomUsersAdmin(admin.ModelAdmin):
+	list_display = ['first_name','last_name','email']
+
+admin.site.register(CustomUser, CustomUsersAdmin)
+admin.site.unregister(Group)
 # Register your models here.
---
+++ authentication/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+path('sign-up/', views.sign_up, name='sign_up'),
+path('login/', views.login_view, name='login')
+]
