1f2850bd56c8ffde7b329dfa1b1e12b4e70d4abf
Thuhaa <josephthuha@gmail.com>
2021-07-06 23:37:33 +0300

    DB Engine

--- eshop/settings.py
+++ eshop/settings.py
@@ -76,17 +76,11 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'eshop',
-        'USER':'postgres',
-        'HOST':'localhost',
-        'PORT':5432,
-        'PASSWORD':'mezenga'
     }
 }
-'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
